f9474deabb01559f4293f425f4b54c0a9ca40650
VidhiB-ITL <vidhi.b@intutechlab.com>
2024-12-08 11:46:06 +0530

    done

---
+++ public/assets/keyspeciaplity.js
@@ -0,0 +1,53 @@
+import first from "../assets/surgery-room 1.png"
+import second from "../assets/surgical gastroenterology.svg"
+import third from "../assets/surgery-room 1.png"
+import fourth from "../assets/medicine 1.png"
+import fifth from "../assets/Robotic_Surgery.png"
+import sixth from "../assets/surgery-room 1.png"
+import seventh from "../assets/surgery-room 1.png"
+import eightth from "../assets/kidneys 1.png"
+import nineth from "../assets/surgery-room 1.png"
+import ten from "../assets/surgery-room 1.png"
+
+export const Speciaplility=[
+
+    {
+        image:first,
+        name:"General & Minimal Access Surgery"
+    },
+    {
+        image:second,
+        name:"Surgical Gastroenterology"
+    },
+    {
+        image:third,
+        name:"Medical Gasroenterology"
+    },
+    {
+        image:fourth,
+        name:"General Medicine"
+    },
+    {
+        image:fifth,
+        name:"Surgical Oncology"
+    },
+    {
+        image:sixth,
+        name:"Medical Oncology"
+    },
+    {
+        image:seventh,
+        name:"Urosurgery"
+    },
+    {
+        image:eightth,
+        name:"Nephrology"
+    },
+    {
+        image:nineth,
+        name:"Orthopaedics & Joint Replacement"
+    }, {
+        image:ten,
+        name:"Neuro & Spine Surgery"
+    },
+]
---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer=()=>{
+    return(
+        <>
+        <img src="https://s3-alpha-sig.figma.com/img/a342/662d/53ff1971a1f65d278eaa1ad6ad7a5ffc?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=q-nCJ6f7e5Mmqh5vMeBBe-bTnSgrMvOqm5NwwCcTs5L8EbXiAPfUtWSjtfOuYLizSWE9Pl2anSRzAnOBMamgABOO~czfZOyG9K9tZbMUWBeMq5TiHimC-~N5BRDmExPu4Ym~o4JUpiL8Z~vfVaMkDvS5IGngAJzh23D4j49-3wRKrQ9QeLo5XhX6yyEi8elqY618WkdlZRvLuP1wjGoOq81SLo3JNbXTMgYjz-Of7HTwl27bQC7XO9qEwyw17r4FLpZXGIb47nGoLK2xMOLQoi1N0rZ8jtlUpAdkcFtZ0l19z-YmPxGIz4nTR0oyWtQvh5PyL0kEi-HuUjVeGV5kpg__"></img>
+        </>
+    )
+}
+export default Footer
